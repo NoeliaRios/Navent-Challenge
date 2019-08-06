6a7fce793410b62ef98fa83e4a944fb3ef2e7475
Glasss87 <44882277+Glasss87@users.noreply.github.com>
2019-08-06 20:13:23 -0300

    fa heart

--- index.js
+++ index.js
@@ -8,10 +8,39 @@ function plusDivs(n) {
 function showDivs(n) {
   var i;
   var x = document.getElementsByClassName("photo");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
+  if (n > x.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = x.length }
   for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
+    x[i].style.display = "none";
   }
-  x[slideIndex-1].style.display = "block";  
+  x[slideIndex - 1].style.display = "block";
 }
+
+
+const likedHeart = document.querySelector('.fa-heart');
+likedHeart.onclick = function(e){
+  e.preventDefault();
+  const heartIcon = document.querySelector('.fa-heart');
+  heartIcon.classList.toggle('.red_heart');
+}
+
+
+
+// var click = 0;
+// var likesPares = 0;
+
+// function likedHeart() {
+
+//   click++
+
+//   if (click % 2 == 0) {
+//     likesPares++
+//     console.log(likesPares)
+//     // var numeroDeLikes = document.querySelector('.fa-heart'); 
+
+//   }
+
+//   var iconos = document.querySelector('.fa-heart')
+//   iconos.classList.toggle('.red_heart')
+
+// }
